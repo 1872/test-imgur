96c69301c3efb5a48144cc156014a692d854ed6c
Peter Jang <peter.x.jang@gmail.com>
2016-05-16 13:16:29 -0500

    imgur upload example

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
+    require 'imgur'
+    client = Imgur::Client.new(ENV['IMGUR_CLIENT'])
+    image = Imgur::LocalImage.new(Rails.root.join("public", "sing-street.jpg").to_s, title: 'Awesome photo')
+    uploaded = client.upload(image)
+    @url = uploaded.link
   end
 end
